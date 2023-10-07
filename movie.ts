2b3d0492e94c640a05cd40581a7e07847bd7798b
Alexey Pozhalov <pozhalov7@mail.ru>
2023-10-07 16:10:26 +0300

    Buyable, movie and cart ts was added

---
+++ movie.ts
@@ -0,0 +1,40 @@
+import Cart from "./cart";
+import {Buyable} from "./buyable";
+
+export default class Movie implements Buyable {
+    constructor(
+        public id: number,
+        public name: string,
+        public year: number,
+        public price: number,
+        public discount?: number,
+        public country: string,
+        public tagline: string,
+        public genres: string[],
+        public duration: number
+    ) {
+    }
+}
+
+// type Movie = {
+//     id: number,
+//     name: string,
+//     year?: number,
+//     country?: string,
+//     tagline?: string,
+//     genre?: string,
+//     time?: number
+// }
+// let movie: Movie = {
+//     id: 124,
+//     name: 'The Avengers',
+//     year: 2012,
+//     country: 'USA',
+//     tagline: '"Avengers Assemble!"',
+//     genre: 'Фантастика, боевик, фэнтези',
+//     time: 137
+// }
+//
+// function add(item): void {
+//     this.items.push(item)
+// }
